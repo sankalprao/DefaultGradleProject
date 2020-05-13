47f74ba310099e4e8fdf6722ee771be2f0439a81
sankalprao <36331783+sankalprao@users.noreply.github.com>
2020-05-13 15:24:29 +1000

    squashed and merge
    * half commit
    
    * full commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+
+    public static void main(){
+        System.out.println("Hello world");
+        System.out.println("Hello world again");
+    }
+}
